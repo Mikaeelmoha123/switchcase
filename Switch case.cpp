9dc08214232b9d149d356ee60e97d60e23841d5b
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-12-03 08:00:21 -0800

    Add files via upload

---
+++ Switch case.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int;
+    cout << "hi, This is your DBIT sem 1 timetable (sep-dec 2021)";
+    cout << "\n1.monday\n2.tuesday\n3.wednesday\n4.thursday\n5.friday" << endl
+         << endl;
+    cout "please select day of choice(e.g 1,2,3, etc...)" << endl;
+
+    switch ()
+    {
+    case 1:
+        cout << "monday classes\n..............." << endl;
+        cout << "\t1. DBIT1106: math for biz" << endl;
+        cout << "\t2. DBIT1102: Intro to programming" << endl;
+        break;
+    case 2:
+        cout << "Tuesday classes\n..............." << endl;
+        cout << "\t2. DBIT1102: intro to programming" << endl;
+        cout << "\t1. DBIT1104: Ethics" << endl;
+        break;
+    }
+}
